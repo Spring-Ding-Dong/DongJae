8b766c0bc908d7e5b63239f28637c46398a3019f
ehdwoKIM <kinbell19@gmail.com>
2025-01-08 00:46:02 +0900

    Initial commit

---
+++ src/main/java/com/springDingDong/dongjae/DongjaeApplication.java
@@ -0,0 +1,13 @@
+package com.springDingDong.dongjae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongjaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongjaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springDingDong/dongjae/DongjaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDingDong.dongjae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongjaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
